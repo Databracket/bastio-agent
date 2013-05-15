3233234252e54fe73035944f643d0ca9315e99f5
Amr Ali <amr@databracket.com>
2013-05-15 04:20:58 +0200

    Import __version__ in bastio/__init__.py
    Signed-off-by: Amr Ali <amr@databracket.com>

---
+++ bastio/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2013 Databracket LLC.
+# See LICENSE file for details.
+
+__author__ = "Amr Ali"
+__copyright__ = "Copyright 2013 Databracket LLC"
+__license__ = "GPLv3+"
+
+"""
+:module: bastio
+:synopsis: Bastio's agent package.
+:author: Amr Ali <amr@databracket.com>
+"""
+
+from .version import __version__

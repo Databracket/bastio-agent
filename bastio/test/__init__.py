96be02d9c0034b42bf74fc5fce50d173a24a874f
Amr Ali <amr@databracket.com>
2013-05-23 02:27:45 +0200

    Add test_ssh_client to test/__init__.py
    Signed-off-by: Amr Ali <amr@databracket.com>

--- bastio/test/__init__.py
+++ bastio/test/__init__.py
@@ -5,6 +5,7 @@ import test_configs
 import test_ssh_crypto
 import test_ssh_protocol
 import test_ssh_api
+import test_ssh_client
 
 def __make_suite(tests):
     suite = unittest.TestSuite()
@@ -19,4 +20,5 @@ suite.addTests(__make_suite(test_configs.tests))
 suite.addTests(__make_suite(test_ssh_crypto.tests))
 suite.addTests(__make_suite(test_ssh_protocol.tests))
 suite.addTests(__make_suite(test_ssh_api.tests))
+suite.addTests(__make_suite(test_ssh_client.tests))
 

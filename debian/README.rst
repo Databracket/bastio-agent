506346d7e37ff51300f07bd34bdd798ad233c19d
Ahmad Aabed <ahmad.aabed.m@gmail.com>
2013-06-11 15:47:41 +0200

    Add builddeb.sh

--- debian/README.rst
+++ debian/README.rst
@@ -1,12 +1,5 @@
 Building deb package
 ====================
 
-* create a top level directoy.
-* cd to it and clone the codebase into opt.
-* cd opt;python setup.py sdist.
-* add etc directory on the same level as opt as you can see.
-* add the init.d files inside the etc directory.
-* add usr directory.
-* add link to /opt/bastio-agent/bin/bastio-agent inside the usr/bin directory
-* cd to the top level directory.
-* dpkg -b bastio bastio-agent_0.1.0_all.deb
+* ./debbuild.sh
+
---
+++ debian/builddeb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf opt/*
+cd ..
+python setup.py sdist 
+cd -
+tar xzf ../dist/Bastio-Agent-0.1.0.tar.gz -C opt
+mv opt/Bastio-Agent-0.1.0/ opt/bastio
+cp -r ../dist/ opt/bastio
+
+if [  -f bastio-agent_0.1.0_all.deb ]; then
+    rm -f bastio-agent_0.1.0_all.deb
+fi
+
+fpm -s dir -t deb -a all -v 0.1.0 -n  bastio-agent  --vendor Databracket --deb-pre-depends 'python-dev' --deb-pre-depends 'python-virtualenv' --deb-pre-depends 'python2.7'  --description 'An agent for Bastio service to provision system accounts and SSH access.' --url 'http://www.bastio.com/' -m 'support@bastio.com'  --license 'GPLv3+'    --deb-user root  --deb-group root  -x builddeb.sh .

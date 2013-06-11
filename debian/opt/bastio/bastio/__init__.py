c9059b6f3cfd00ccb9cb248e468fa37b90a35d0c
Ahmad Aabed <ahmad.aabed.m@gmail.com>
2013-06-11 15:50:41 +0200

    Add opt directory

---
+++ debian/opt/bastio/bastio/__init__.py
@@ -0,0 +1,54 @@
+# Copyright 2013 Databracket LLC.
+# See LICENSE file for details.
+
+__author__ = "Amr Ali"
+__copyright__ = "Copyright 2013 Databracket LLC"
+__license__ = "GPLv3+"
+
+"""
+:module: bastio
+:synopsis: Bastio's agent package.
+:author: Amr Ali <amr@databracket.com>
+
+CLI Management
+--------------
+.. automodule:: bastio.cli
+
+SSH And Communication Management
+--------------------------------
+.. automodule:: bastio.ssh
+
+Account Operations
+------------------
+.. automodule:: bastio.account
+
+Configuration Utilities
+-----------------------
+.. automodule:: bastio.configs
+
+Concurrency Utilities
+---------------------
+.. automodule:: bastio.concurrency
+
+Logging Facility
+----------------
+.. automodule:: bastio.log
+
+Mixins And Language Helpers
+---------------------------
+.. automodule:: bastio.mixin
+
+Version Information
+-------------------
+.. automodule:: bastio.version
+
+Exceptions
+----------
+.. automodule:: bastio.excepts
+"""
+
+from .version import __version__, __protocol__
+
+def load_test_suite():
+    from bastio.test import suite
+    return suite
---
+++ debian/opt/bastio/bastio/log.py
@@ -0,0 +1,66 @@
+# Copyright 2013 Databracket LLC
+# See LICENSE file for details.
+
+__author__ = "Amr Ali"
+__copyright__ = "Copyright 2013 Databracket LLC"
+__license__ = "GPLv3+"
+
+"""
+:module: bastio.log
+:synopsis: Logging facilities for this project.
+:author: Amr Ali <amr@databracket.com>
+
+Facilities
+----------
+.. autoclass:: Logger
+    :members:
+"""
+
+import logging
+
+from bastio.mixin import public, UniqueSingletonMeta
+
+def _getSyslogAddress():
+    import sys
+
+    if sys.platform.startswith('linux'):
+        return '/dev/log'
+    elif sys.platform.startswith('darwin'):
+        return '/var/run/syslog'
+
+@public
+class Logger(object):
+    """A class that provides logging facilities exposed by the underlying
+    Python's logging module.
+    """
+    __metaclass__ = UniqueSingletonMeta
+
+    def __init__(self, prog = 'bastio-agent'):
+        self._logger = logging.getLogger(prog)
+        self._formatter = logging.Formatter(prog + '[%(process)d]: %(message)s')
+        self._logger.addHandler(logging.NullHandler())
+        self._syslog_enabled = False
+        self._stream_enabled = False
+
+    def __getattr__(self, name):
+        return getattr(self._logger, name)
+
+    def enable_syslog(self):
+        """Enable Syslog logging. This method is idempotent."""
+        if self._syslog_enabled:
+            return
+        _handler = SysLogHandler(address=_getSyslogAddress(),
+                facility=logging.SysLogHandler.LOG_SYSLOG)
+        _handler.setFormatter(self._formatter)
+        self._logger.addHandler(_handler)
+        self._syslog_enabled = True
+
+    def enable_stream(self):
+        """Enable logging to STDOUT. This method is idempotent."""
+        if self._stream_enabled:
+            return
+        _handler = logging.StreamHandler()
+        _handler.setFormatter(self._formatter)
+        self._logger.addHandler(_handler)
+        self._stream_enabled = True
+

8191462eee4abb8623594f33ac30551fc7b85e33
Ahmad Aabed <ahmad.aabed.m@gmail.com>
2013-06-21 04:05:52 +0200

    Add docstring to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import *
 from bastio import __version__
 def builddeb():
+	"""Creates a new debian package"""
 	local('python setup.py sdist')
 	with settings(warn_only=True):   
 		local('dpkg-buildpackage -tc',capture=False)
@@ -8,10 +9,12 @@ def builddeb():
 	#local('mv ../*.changes	.')
 	
 def clean():
+	"""Cleans debian package and runs setup.py clean"""
 	local('rm -f bastio-agent_%s_all.deb' %__version__)
 	local('python setup.py clean')
 
 def release(name="Ahmad Aabed",email="ahmad.aabed.m@gmail.com",):
+	"""Creates a release package i.e (opens the changelog file to write changes)"""
 	print __version__
 	with shell_env(DEBFULLNAME=name, DEBEMAIL=email):
             local('dch -v %s' %__version__)

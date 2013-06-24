5b1530f8c84354785399fcf1c46ade304155fabc
Amr Ali <amr@databracket.com>
2013-06-24 03:30:34 +0200

    Clean up fabfile
    Signed-off-by: Amr Ali <amr@databracket.com>

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,19 @@
+# Copyright 2013 Databracket LLC.
+# See LICENSE for details.
+
 from fabric.api import *
 from bastio import __version__
-def builddeb():
-	"""Creates a new debian package"""
-	local('python setup.py sdist')
-	with settings(warn_only=True):   
-		local('dpkg-buildpackage -tc',capture=False)
-	local('mv ../bastio-agent_%s_all.deb .' %__version__)
-	#local('mv ../*.changes	.')
-	
-def clean():
-	"""Cleans debian package and runs setup.py clean"""
-	local('rm -f bastio-agent_%s_all.deb' %__version__)
-	local('python setup.py clean')
 
-def release(name="Ahmad Aabed",email="ahmad.aabed.m@gmail.com",):
-	"""Creates a release package i.e (opens the changelog file to write changes)"""
-	print __version__
-	with shell_env(DEBFULLNAME=name, DEBEMAIL=email):
-            local('dch -v %s' %__version__)
+def builddeb():
+    """Create a new debian package"""
+    local('python setup.py sdist')
+    local('git log --stat > debian/changelog')
+    with settings(warn_only=True):
+        local('dpkg-buildpackage -A -tc', capture=False)
+    local('mv ../bastio-agent_{}_all.deb .'.format(__version__))
+
+def clean():
+    """Clean debian package and sdist artifacts"""
+    local('rm -f bastio-agent_{}_all.deb'.format(__version__))
+    local('python setup.py clean')
+

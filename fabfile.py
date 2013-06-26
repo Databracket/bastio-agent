876f61b88b9f78d4fa9428b75863dfcdab5fde73
Amr Ali <amr@databracket.com>
2013-06-26 02:56:24 +0200

    Fix builddeb and clean functions and add uploaddeb in fabfile
    Signed-off-by: Amr Ali <amr@databracket.com>

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,17 @@
 # See LICENSE for details.
 
 import os
+import tempfile
 import platform
-
+import shutil
 from fabric.api import *
+
 from bastio import __version__
+
+ROOT_DIR = os.path.dirname(__file__)
+DIST_DIR = os.path.join(ROOT_DIR, 'dist')
+DEBIAN_DIR = os.path.join(ROOT_DIR, 'debian')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
 def __suppress(func, *args, **kwargs):
     try:
@@ -13,22 +20,52 @@ def __suppress(func, *args, **kwargs):
     except Exception as ex:
         return ex
 
-def builddeb(name="Bastio (Databracket LLC Bastio Package Signing Key)",
-        email="databracket@bastio.com", dist=None):
+def builddeb(name, email, dist=None, pkg_version='1', keyid='599FA492'):
     """Create a new debian package"""
     dist = dist if dist else platform.linux_distribution()[2]
-    local('python setup.py sdist')
-    __suppress(os.unlink, "debian/changelog")
-    with shell_env(DEBFULLNAME=name, DEBEMAIL=email):
-        local('dch --package bastio-agent --distribution {} --create -v {}'.format(
-            dist, __version__))
-    with settings(warn_only=True):
-        local('dpkg-buildpackage -tc -S -k599FA492', capture=False)
-    local('mv ../bastio-agent_{}_all.deb .'.format(__version__))
-    local('mv ../bastio-agent_{}_*.changes .'.format(__version__))
+    version = "{}-{}".format(__version__, pkg_version)
+    dch_args = ['dch']
+    if os.path.exists(os.path.join(DEBIAN_DIR, 'changelog')):
+        dch_args.append('-i')
+    else:
+        dch_args.append('--create')
+        dch_args.append('--package bastio-agent')
+        dch_args.append('--distribution {}'.format(dist))
+        dch_args.append('-v {}'.format(version))
+
+    # Add an entry to the changelog and prepare the distribution tar-ball
+    with lcd(ROOT_DIR):
+        local('python setup.py sdist')
+        with shell_env(DEBFULLNAME=name, DEBEMAIL=email):
+            local(' '.join(dch_args))
+
+    # Prepare the source package environment
+    tempdir = tempfile.mkdtemp()
+    environdir = os.path.join(tempdir, 'environ')
+    os.mkdir(environdir)
+    shutil.copytree(DEBIAN_DIR, os.path.join(environdir, 'debian'))
+    shutil.copytree(DATA_DIR, os.path.join(environdir, 'data'))
+    shutil.copytree(DIST_DIR, os.path.join(environdir, 'dist'))
+
+    # Build a source package
+    with lcd(environdir):
+        local('dpkg-buildpackage -S -tc -k{}'.format(keyid), capture=False)
+
+    # Move result to the root directory of the repository
+    with lcd(tempdir):
+        local('mv bastio-agent_{}* {}'.format(__version__, ROOT_DIR))
+
+    # Clean up the temporary environment
+    __suppress(shutil.rmtree, tempdir)
+
+def uploaddeb(ppa="ppa:databracket/bastio-agent"):
+    """Upload the latest source package to Launchpad"""
+    with lcd(ROOT_DIR):
+        local("dput {} bastio-agent_{}*_source.changes".format(ppa, __version__))
 
 def clean():
     """Clean debian package and sdist artifacts"""
-    local('rm -f bastio-agent_{}_all.deb'.format(__version__))
-    local('python setup.py clean')
+    with lcd(ROOT_DIR):
+        local('rm -f bastio-agent_{}*'.format(__version__))
+        local('python setup.py clean')
 
